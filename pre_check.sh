b32b5afed63434449119ae3edeff767435e5f36d
masashi <wf9a5m75@gmail.com>
2023-01-07 14:16:08 -0800

    fix: can't update the README.md

---
+++ pre_check.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NEW_FILES=$(git log --after 'yesterday' --invert-grep --author='action@github.com' | wc -l)
+if [[ $NEW_FILES -gt 0 ]]; then
+  echo "DO_UPDATE=1" >> $GITHUB_OUTPUT
+else
+  echo "DO_UPDATE=0" >> $GITHUB_OUTPUT
+fi
