0e5fe68d1e2f43eec9af1760ba47565f4c59e5ba
Masashi Katsumata <wf9a5m75@gmail.com>
2023-01-09 16:39:04 -0800

    github actions: test

--- pre_check.sh
+++ pre_check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #NEW_FILES=$(git log --after 'yesterday' --invert-grep --author='action@github.com')
-git log --after 'yesterday' --invert-grep --author='action@github.com'
+git log --after 'yesterday'
 
 # if [[ $NEW_FILES -gt 0 ]]; then
 #   echo "DO_UPDATE=1" >> $GITHUB_OUTPUT
