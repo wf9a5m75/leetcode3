3853efa903e40aedabc6afbb950f755aca331323
Masashi Katsumata <wf9a5m75@gmail.com>
2023-01-09 16:15:12 -0800

    actions: test

--- pre_check.sh
+++ pre_check.sh
@@ -3,7 +3,7 @@
 git log --after 'yesterday' --invert-grep --author='action@github.com'
 
 NEW_FILES=$(git log --after 'yesterday' --invert-grep --author='action@github.com' | wc -l)
-echo $NEW_FILES
+git log --after 'yesterday' --invert-grep --author='action@github.com' 
 if [[ $NEW_FILES -gt 0 ]]; then
   echo "DO_UPDATE=1" >> $GITHUB_OUTPUT
 else
