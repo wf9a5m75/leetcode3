f25d7c0830f5916a9886a760af652638f2f0a9bc
Masashi Katsumata <wf9a5m75@gmail.com>
2023-01-09 16:52:39 -0800

    fix: pre_check.sh does not work correctly.

--- pre_check.sh
+++ pre_check.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-#NEW_COMMITS=$(git log --after 'yesterday' --invert-grep --author='action@github.com')
-git version
-NEW_COMMITS=$(git log --since 'yesterday' --invert-grep --author='action@github.com' | wc -l )
-echo "NEW COMMITS: $NEW_COMMITS"
-if [[ $NEW_COMMITS -gt 0 ]]; then
+NEW_FILES=$(find ./*/* \( -name "*.kt" -or -name "*.py" \) -a -mtime -1 | wc -l)
+echo "NEW_FILES: $NEW_FILES"
+if [[ $NEW_FILES -gt 0 ]]; then
   echo "DO_UPDATE=1" >> $GITHUB_OUTPUT
 else
   echo "DO_UPDATE=0" >> $GITHUB_OUTPUT
