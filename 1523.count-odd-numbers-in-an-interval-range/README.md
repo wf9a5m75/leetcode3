915812b468d45533bae8d9dcc600f7bedab22365
masashi katsumata <wf9a5m75@gmail.com>
2023-08-30 16:19:47 -0700

    Revert "1523. Count Odd Numbers in an Interval Range"
    This reverts commit 79d29ad1100c3c3bd47a095f62f2b34b19b0a205.

--- 1523.count-odd-numbers-in-an-interval-range/README.md
+++ 1523.count-odd-numbers-in-an-interval-range/README.md
@@ -1,6 +1,6 @@
 # 1523. Count Odd Numbers in an Interval Range
 ## level: Easy
 
-- tag: Programming Skills
+- tag: Programming Skills I
 
 https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
--- 1523.count-odd-numbers-in-an-interval-range/solution.py
+++ 1523.count-odd-numbers-in-an-interval-range/solution.py
@@ -1,17 +1,13 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        count = 0
-        
-        if high & 1 == 1:
-            count += 1
-            high -= 1
-        
-        if (low >= high):
-            return count
-        
-        if low & 1 == 1:
-            count += 1
+        result = 0
+        if (low & 1) == 1:
+            result += 1
             low += 1
-        
-        count += (high - low) >> 1
-        return count
+        if (high & 1) == 1:
+            result += 1
+            high -= 1
+
+        result += (high - low) >> 1
+        return result
+    
