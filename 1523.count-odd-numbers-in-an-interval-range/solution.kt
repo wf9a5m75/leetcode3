79d29ad1100c3c3bd47a095f62f2b34b19b0a205
masashi katsumata <wf9a5m75@gmail.com>
2023-08-30 16:16:19 -0700

    1523. Count Odd Numbers in an Interval Range

--- 1523.count-odd-numbers-in-an-interval-range/solution.kt
+++
@@ -1,9 +0,0 @@
-class Solution {
-    fun countOdds(low: Int, high: Int): Int {
-        if (low.rem(2) == 0 && high.rem(2) == 0) {
-            return (high - low) / 2
-        } else {
-            return (high - low) / 2 + 1
-        }
-    }
-}
--- 1523.count-odd-numbers-in-an-interval-range/solution.py
+++ 1523.count-odd-numbers-in-an-interval-range/solution.py
@@ -1,13 +1,17 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        result = 0
-        if (low & 1) == 1:
-            result += 1
-            low += 1
-        if (high & 1) == 1:
-            result += 1
+        count = 0
+        
+        if high & 1 == 1:
+            count += 1
             high -= 1
-
-        result += (high - low) >> 1
-        return result
-    
+        
+        if (low >= high):
+            return count
+        
+        if low & 1 == 1:
+            count += 1
+            low += 1
+        
+        count += (high - low) >> 1
+        return count
