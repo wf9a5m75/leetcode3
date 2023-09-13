54fb09d8afe90ab2e836cf45c2ccc9ba454acf50
masashi katsumata <wf9a5m75@gmail.com>
2023-09-12 19:37:44 -0700

    add 1647. Minimum Deletions to Make Character Frequencies Unique

--- 1643.minimum-deletions-to-make-character-frequencies-unique/solution.py
+++ 1643.minimum-deletions-to-make-character-frequencies-unique/solution.py
@@ -1,6 +1,21 @@
 class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        richest = 0
-        for account in accounts:
-            richest = max(richest, sum(account))
-        return richest
+    def minDeletions(self, s: str) -> int:
+        
+        counts = defaultdict(int)
+        for char in s:
+            counts[char] += 1
+        
+        hq = []
+        for char, cnt in counts.items():
+            heapq.heappush(hq, -cnt)
+        
+        delete = 0
+        while (len(hq) > 0):
+            peakCnt = heapq.heappop(hq)
+            while (len(hq) > 0) and (hq[0] == peakCnt):
+                delete += 1
+                cnt = heapq.heappop(hq)
+                if (cnt + 1 == 0):
+                    continue
+                heapq.heappush(hq, cnt + 1)
+        return delete
