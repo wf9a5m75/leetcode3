8ff244d2399d652b1ab973e0d4f53b7ae948386b
masashi katsumata <wf9a5m75@gmail.com>
2023-09-13 18:24:58 -0700

    146. LRU Cache

---
+++ 146.lru-cache/README.md
@@ -0,0 +1,4 @@
+# 146. LRU Cache
+## level: Medium
+
+https://leetcode.com/problems/lru-cache/
---
+++ 146.lru-cache/solution.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = []
+        results = []
+        while(stack or root):
+            while (root):
+                results.append(root.val)
+                stack.append(root)
+                root = root.left
+
+            if stack:
+                root = stack.pop()
+                root = root.right
+        return results
