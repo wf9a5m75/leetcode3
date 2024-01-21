a7ff36f3d048c2793d3f85f161b8f87188867f95
masashi katsumata <wf9a5m75@gmail.com>
2024-01-21 00:54:44 -0800

    add: 198. House Robber

---
+++ 198.house-robber/solution5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        N = len(nums)
+        if (N == 1):
+            return nums[0]
+        
+        dp = [0] * N
+        dp[0] = nums[0]
+        dp[1] = max(dp[0], nums[1])
+
+        for i in range(2, N):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        return dp[N - 1]
