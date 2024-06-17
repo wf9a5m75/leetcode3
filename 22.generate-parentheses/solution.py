178b81783857d1a2735b79e4e68add30166ba23c
masashi katsumata <wf9a5m75@gmail.com>
2024-06-17 12:27:47 -0700

    Add python3 solution for 22.Generate Parentheses

---
+++ 22.generate-parentheses/solution.py
@@ -0,0 +1,24 @@
+from typing import List, Dict
+
+class Solution:
+    def __init__(self):
+        self.parenthesis_cache: Dict[int, str] = {}
+        
+    def generateParenthesis(self, n: int) -> List[str]:
+        if (n == 0):
+            return [""]
+        
+        if (n in self.parenthesis_cache):
+            return self.parenthesis_cache[n]
+        
+        result: List[str] = []
+        
+        for left_count in range(n):
+            for left_hand in self.generateParenthesis(left_count):
+                
+                right_count = n - 1 - left_count
+                for right_hand in self.generateParenthesis(right_count):
+                    result.append(f"({left_hand})" + right_hand)
+        
+        self.parenthesis_cache[n] = result
+        return result
