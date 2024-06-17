fd91a500e50dabb1ceebfb8ada000b1fd6e390da
masashi katsumata <wf9a5m75@gmail.com>
2024-06-17 14:24:10 -0700

    739. Daily Temperatures

---
+++ 739.daily-temperatures/solution1.py
@@ -0,0 +1,21 @@
+from collections import deque
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        number_of_days = len(temperatures)
+        
+        results = [0] * number_of_days
+        
+        # Store index of the higher days
+        past_days = deque()
+        
+        for today_idx in range(number_of_days):
+            today = temperatures[today_idx]
+            
+            # Update the results where the past day lower temperature than today.
+            while (len(past_days) > 0) and (temperatures[past_days[-1]] < today):
+                past_day_idx = past_days.pop()
+                num_of_days = today_idx - past_day_idx
+                results[past_day_idx] = num_of_days
+            
+            past_days.append(today_idx)
+        return results
