fd916d01b6cc457b30cc12b7f95971757e76d660
Masashi Katsumata <wf9a5m75@gmail.com>
2024-06-17 19:21:46 -0700

    Fix: pep8 linter errors

--- 22.generate-parentheses/solution.py
+++ 22.generate-parentheses/solution.py
@@ -4,6 +4,7 @@ class Solution:
     def __init__(self):
         self.parenthesis_cache: Dict[int, str] = {}
 
+
     def generateParenthesis(self, n: int) -> List[str]:
         """
         Logic:
@@ -11,32 +12,31 @@ class Solution:
             n = 1  
                 ("") + "" -> ["(A)"]
             n = 2
-               ("") + (A) -> ["(C)(A)"]
-               ((A)) + "" -> ["((B))"]
+                ("") + (A) -> ["(C)(A)"]
+                ((A)) + "" -> ["((B))"]
             n = 3
                 ("") + (C)(A) -> ["()()()"]
                 ("") + ((B)) -> ["()(())"]
-                
+
                 ((A)) + (A) -> ["(())()"]
-                
+
                 (((B))) + "" -> ["((()))"]
                 ((C)(A)) + "" -> ["(()())"]
-                
         """
         if (n == 0):
             return [""]  # seed
-        
+
         if (n in self.parenthesis_cache):
             return self.parenthesis_cache[n]
-        
+
         results: List[str] = []
-        
+
         for left_count in range(n):
             for left_hand in self.generateParenthesis(left_count):
-                
+
                 right_count = n - 1 - left_count
                 for right_hand in self.generateParenthesis(right_count):
                     results.append(f"({left_hand})" + right_hand)
-        
+
         self.parenthesis_cache[n] = results
         return results
