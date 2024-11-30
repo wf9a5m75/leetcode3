494a9f73808c398ae7c32205fcb0813f06194a2c
Masashi Katsumata <wf9a5m75@gmail.com>
2024-11-30 13:55:30 -0800

    167. Two Sum II - Input Array Is Sorted

---
+++ 167.two-sum-ii-input-array-is-sorted/README.md
@@ -0,0 +1,6 @@
+# 167. Two Sum II - Input Array Is Sorted
+## level: Medium
+
+- tag: Neetcode 150
+
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
---
+++ 167.two-sum-ii-input-array-is-sorted/solution.ts
@@ -0,0 +1,16 @@
+function twoSum(numbers: number[], target: number): number[] {
+  let left = 0;
+  let right = numbers.length - 1;
+  while (left < right) {
+      const sum = numbers[left] + numbers[right];
+      if (sum === target) {
+          break;
+      }
+      if (sum < target) {
+          left++;
+      } else {
+          right--;
+      }
+  }
+  return [left + 1, right + 1];
+};
