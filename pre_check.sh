219cbcd01d91d68f256e451ea2b03d76f575159e
Masashi Katsumata <wf9a5m75@gmail.com>
2024-12-22 15:26:37 -0800

    update: pre_check.sh

--- pre_check.sh
+++ pre_check.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-NEW_FILES=$(find ./*/* \( -name "*.kt" -or -name "*.py" \) -a -mtime -1 | wc -l)
-echo "NEW_FILES: $NEW_FILES"
+# ファイルの最終更新日時を取得
+last_commit_date=$(git log -1 --format="%ci" -- README.md)
+
+# 現在の日付と比較して何日前かを計算
+days_ago=$(( ($(date -d "$last_commit_date" +%s) - $(date +%s)) / 86400 ))
+
+NEW_FILES=$(find ./*/* \( -name "*.kt" -or -name "*.py" \) -a -mtime $days_ago | wc -l)
 if [[ $NEW_FILES -gt 0 ]]; then
   echo "DO_UPDATE=1" >> $GITHUB_OUTPUT
 else
