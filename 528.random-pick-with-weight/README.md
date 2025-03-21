8a9cc166782f800cbaff03ac5593d7a8a64c69ac
Masashi Katsumata <wf9a5m75@gmail.com>
2025-03-21 13:48:23 -0700

    Add: 528. Random Pick with Weight

---
+++ 528.random-pick-with-weight/README.md
@@ -0,0 +1,6 @@
+# 528. Random Pick with Weight
+## level: Medium
+
+- tag: meta
+
+https://leetcode.com/problems/random-pick-with-weight
---
+++ 528.random-pick-with-weight/solution.ts
@@ -0,0 +1,25 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.prefix_sums = []
+        prefix_sum = 0
+        for weight in w:
+            prefix_sum += weight
+            self.prefix_sums.append(prefix_sum)
+        self.total_sum = prefix_sum
+
+    def pickIndex(self) -> int:
+        target = self.total_sum * random.random()
+        low, high = 0, len(self.prefix_sums)
+        while low < high:
+            mid = (low + high) >> 1
+            if self.prefix_sums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid
+        return low
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
